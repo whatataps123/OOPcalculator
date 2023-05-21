1d3438973f809ad0b1d225ce173d46201f00f23f
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-05-21 22:52:06 +0800

    calc operation with error handling done

--- userinterface.py
+++ userinterface.py
@@ -5,5 +5,15 @@ class UserInterface:
         print("[2] Subtraction")
         print("[3] Multiplication")
         print("[4] Division")
-
+        while True:
+            try:
+                calc_operation = int(input("\nEnter the number of your desired operation (1-4): "))
+                if calc_operation < 1 or calc_operation > 4:
+                    raise ValueError("Invalid operation")
+                break
+            except ValueError:
+                print("\033[1;31m" + "Invalid input for the operation. Please enter a number from 1 to 4. Try again." + "\033[1;m")
+                
+        return calc_operation
+        
     
