567e110160641dd15af5847c55d9e09bfce63c42
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-05-21 22:29:47 +0800

    added userinterface file

---
+++ mainprogram.py
@@ -0,0 +1,16 @@
+# Joshua Lemuel Z. Centina
+# BS CpE 1-4
+from userinterface import UserInterface
+# Convert your previous calculator program into an OOP type of program.
+ui = UserInterface()
+# pseudocode
+# Ask user what operator will be used
+ui.user_choose()
+# Insert handling errors
+# Ask user for num1
+# Insert handling errors
+# Ask user num2
+# Insert handling errors
+# Print the output
+# Ask the user again if they want to try again (y/n)
+# Insert handling errors
---
+++ userinterface.py
@@ -0,0 +1,9 @@
+class UserInterface:
+    def user_choose(self):
+        print("Choose your desired operation:")
+        print("[1] Addition")
+        print("[2] Subtraction")
+        print("[3] Multiplication")
+        print("[4] Division")
+
+    
