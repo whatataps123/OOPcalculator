c2d9f44ce9470f07d42bcc34a0ae5efd770f5f31
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-05-23 07:23:36 +0800

    num2 handling errors done

--- mainprogram.py
+++ mainprogram.py
@@ -3,16 +3,15 @@
 from userinterface import UserInterface
 # Convert your previous calculator program into an OOP type of program.
 ui = UserInterface()
-operation = ui.user_choose()
 # pseudocode
 # Ask user what operator will be used
-ui.user_choose()
+operation = ui.user_choose()
 # Insert handling errors
 # Ask user for num1
-ui.input_num1()
+num1 = ui.input_num1()
 # Insert handling errors
 # Ask user num2
-ui.input_num2(operation)
+num2 = ui.input_num2(operation)
 # Insert handling errors
 # Print the output
 # Ask the user again if they want to try again (y/n)
