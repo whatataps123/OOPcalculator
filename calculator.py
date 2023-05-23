9e490eb4089a37414ed2991e3aebdeda9a791c85
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-05-23 08:26:55 +0800

    added print function in class Calculator

--- calculator.py
+++ calculator.py
@@ -1,12 +1,16 @@
 class Calculator:
     def add(self, num_1, num_2):
+        print(num_1, "+", num_2)
         return num_1 + num_2
     
     def subtract(self, num_1, num_2):
+        print(num_1, "-", num_2)
         return num_1 - num_2
     
     def multiply(self, num_1, num_2):
+        print(num_1, "*", num_2)
         return num_1 * num_2
     
     def divide(self, num_1, num_2):
+        print(num_1, "/", num_2)
         return num_1 / num_2
