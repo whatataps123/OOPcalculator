d5d52c871ca424f26a9ba20b43f458b2a7cb54a3
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-05-23 08:29:38 +0800

    added color while printing the output

--- mainprogram.py
+++ mainprogram.py
@@ -8,21 +8,21 @@ from calculator import Calculator
 ui = UserInterface()
 calc = Calculator()
 
-print("=============================================================================")
-def typewriter(text, delay=0.1):
-  for letter in text:
-    print(letter, end='', flush=True)
-    time.sleep(delay)
-  print()
-typewriter("\033[1;46m" + "Welcome to Joshua Lemuel Z. Centina's Calculator" + "\033[1;m")
+# print("=============================================================================")
+# def typewriter(text, delay=0.1):
+#   for letter in text:
+#     print(letter, end='', flush=True)
+#     time.sleep(delay)
+#   print()
+# typewriter("\033[1;46m" + "Welcome to Joshua Lemuel Z. Centina's Calculator" + "\033[1;m")
 
-print("\nLoading:")
-animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
-for i in range(len(animation)):
-    time.sleep(0.2)
-    sys.stdout.write("\r" + animation[i % len(animation)])
-    sys.stdout.flush()
-print("\n")
+# print("\nLoading:")
+# animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+# for i in range(len(animation)):
+#     time.sleep(0.2)
+#     sys.stdout.write("\r" + animation[i % len(animation)])
+#     sys.stdout.flush()
+# print("\n")
 
 #Start
 while True:
@@ -31,18 +31,18 @@ while True:
     num_2 = ui.input_num2(operation)
     if operation == 1:
         sum = calc.add(num_1,num_2)
-        print(sum)
+        print("The sum:", "\033[1;43m" + str(sum) + "\033[1;m")
 
     elif operation == 2:
         diff = calc.subtract(num_1,num_2)
-        print(diff)
+        print("The difference:", "\033[1;43m" + str(diff) + "\033[1;m")
 
     elif operation == 3:
         product = calc.multiply(num_1,num_2)
-        print(product)
+        print("The product:", "\033[1;43m" + str(product) + "\033[1;m")
 
     elif operation == 4:
         quotient = calc.divide(num_1,num_2)
-        print(quotient)
+        print("The quotient:", "\033[1;43m" + str(quotient) + "\033[1;m")
     if not ui.retry():
         break
