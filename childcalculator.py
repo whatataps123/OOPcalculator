e4978f794e905ee528deb17f72db0bf03e307ebd
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-06-03 13:10:03 +0800

    Done doing inheritence, override and added new methods

--- childcalculator.py
+++ childcalculator.py
@@ -1,5 +1,17 @@
 from calculator import Calculator
 import math
 class ChildCalculator(Calculator):
+    def add(self):
+        print("Override testing")
+
     def sqrtAdd(self, sum):
         return math.sqrt(sum)
+    
+    def sqrtSub(self, diff):
+        return math.sqrt(diff)
+
+    def sqrtMult(self, product):
+        return math.sqrt(product)
+    
+    def sqrtDiv(self, quotient):
+        return math.sqrt(quotient)
--- mainprogram.py
+++ mainprogram.py
@@ -10,21 +10,21 @@ ui = UserInterface()
 calc = Calculator()
 child = ChildCalculator()
 
-# print("=============================================================================")
-# def typewriter(text, delay=0.1):
-#   for letter in text:
-#     print(letter, end='', flush=True)
-#     time.sleep(delay)
-#   print()
-# typewriter("\033[1;46m" + "Welcome to Joshua Lemuel Z. Centina's Calculator" + "\033[1;m")
+print("=============================================================================")
+def typewriter(text, delay=0.1):
+  for letter in text:
+    print(letter, end='', flush=True)
+    time.sleep(delay)
+  print()
+typewriter("\033[1;46m" + "Welcome to Joshua Lemuel Z. Centina's Calculator" + "\033[1;m")
 
-# print("\nLoading:")
-# animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
-# for i in range(len(animation)):
-#     time.sleep(0.2)
-#     sys.stdout.write("\r" + animation[i % len(animation)])
-#     sys.stdout.flush()
-# print("\n")
+print("\nLoading:")
+animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+for i in range(len(animation)):
+    time.sleep(0.2)
+    sys.stdout.write("\r" + animation[i % len(animation)])
+    sys.stdout.flush()
+print("\n")
 
 #Start
 while True:
@@ -32,21 +32,30 @@ while True:
     num_1 = ui.input_num1()
     num_2 = ui.input_num2(operation)
     if operation == 1:
+        child.add()
         sum = calc.add(num_1,num_2)
         sqrtAdd = child.sqrtAdd(sum)
+        child.add()
         print("The sum:", "\033[1;43m" + str(sum) + "\033[1;m")
         print("The square root is", sqrtAdd)
 
     elif operation == 2:
         diff = calc.subtract(num_1,num_2)
+        sqrtSub = child.sqrtSub(diff)
         print("The difference:", "\033[1;43m" + str(diff) + "\033[1;m")
+        print("The square root is", sqrtSub)
 
     elif operation == 3:
         product = calc.multiply(num_1,num_2)
+        sqrtMult = child.sqrtMult(product)
         print("The product:", "\033[1;43m" + str(product) + "\033[1;m")
+        print("The square root is:", sqrtMult)
 
     elif operation == 4:
         quotient = calc.divide(num_1,num_2)
+        sqrtDiv = child.sqrtDiv(quotient)
         print("The quotient:", "\033[1;43m" + str(quotient) + "\033[1;m")
+        print("The square root is:", sqrtDiv)
+
     if not ui.retry():
         break
